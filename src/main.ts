0fea07eb6c1a8fdca7107eb54e0d38a8990bb38f
Jeroen Peeters <jeroen.peeters@evance.be>
2018-08-22 17:50:43 +0200

    feat: angular-parcel init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Bootstrap the application
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+// Local dependancies
+import { AppModule } from "./app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
